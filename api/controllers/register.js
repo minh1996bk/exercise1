44cf96dfdb8f0c58339235af0fe7848c9f91b56b
minh1996bk <minh1996bk@gmail.com>
2018-04-12 11:12:36 +0700

    them action register

---
+++ api/controllers/register.js
@@ -0,0 +1,24 @@
+module.exports = async function doRegister(req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let bcrypt = require('bcrypt');
+
+    let hashPassword = await bcrypt.hash(password, 10);
+
+    let admin =  Admin.create({
+        username: username,
+        password: password
+    })
+    .fetch();
+
+    if (admin) {
+        res.json({
+            message: "register success"
+        })
+    } else {
+        res.json({
+            message: "smt faild"
+        })
+    }
+}

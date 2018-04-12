7b3afcbd182f532c53dd4fa877224f574d4a89c6
minh1996bk <minh1996bk@gmail.com>
2018-04-12 11:13:52 +0700

    update action login

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -2,7 +2,7 @@ module.exports = async function doLogin(req, res) {
     let username = req.body.username;
     let password = req.body.password;
 
-    // let bcrypt = require('bcrypt');
+    let bcrypt = require('bcrypt');
     try {
         let user = await Admin.findOne({
             username: username
@@ -12,8 +12,8 @@ module.exports = async function doLogin(req, res) {
                 message: 'username not exists'
             })
         }
-        // let result = await bcrypt.compare(password, user.password);
-        if (user.password == password) {
+        let result = await bcrypt.compare(password, user.password);
+        if (result) {
             req.session.userId = user.id;
             return res.json({
                 message: 'login success'

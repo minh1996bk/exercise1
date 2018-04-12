ab3fd9e00f7429c9a6f01ec37d36e6d8b8c5788f
ThaoThu <thaothung.04@gmail.com>
2018-04-12 16:33:22 +0700

    lam giao dien updaate student

--- api/controllers/students.js
+++ api/controllers/students.js
@@ -1,3 +1,23 @@
 module.exports = async function getStudents(req, res) {
+    let students = [
+        {
+            mssv: "2014",
+            name: "minh",
+            gender: "nam",
+            dateOfBirth: "201233",
+            address: "adsfasidfj",
+        },
+        {
+            mssv: "2014",
+            name: "minh",
+            gender: "name",
+            dateOfBirth: "201233",
+            address: "adsfasidfj",
+        },
 
+
+    ]
+    res.view('pages/students', {
+        students: students
+    });
 }

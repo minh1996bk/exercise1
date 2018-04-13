a3cc9dc6fc599046ee86ba5e5e503c1e58057d6c
minh1996bk <minh1996bk@gmail.com>
2018-04-13 21:26:48 +0700

    xu ly controler get and post student

--- api/controllers/StudentController.js
+++ api/controllers/StudentController.js
@@ -1,9 +1,33 @@
 module.exports = {
-    get: function getStudent(req, res) {
-        res.render("pages/students");
+    get: async function getStudent(req, res) {
+        let studentId = req.params.studentId;
+        let student = await Student.findOne({'id': studentId});
+        res.view('pages/update-student', {student: student});
     },
-    post: function addStudent(req, res) {
-        
+    post: async function addStudent(req, res) {
+        let _mssv = req.body.mssv;
+        let _name = req.body.name;
+        let _dateOfBirth = req.body.dateOfBirth;
+        let _gender = req.body.gender;
+        let _address = req.body.address;
+
+        let student = await Student.create({
+            mssv: _mssv,
+            name: _name,
+            dateOfBirth: _dateOfBirth,
+            gender: _gender,
+            address: _address
+        })
+        .fetch();
+        if (student) {
+            res.status(200).json({
+                message: "OK"
+            })
+        } else {
+            res.status(500).json({
+                message: "Fail"
+            })
+        }
     },
     put: function updateStudent(req, res) {
         

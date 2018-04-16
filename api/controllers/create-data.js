9226df7096185432a7acffda9d9badf6d1262b5d
minh1996bk <minh1996bk@gmail.com>
2018-04-16 20:49:30 +0700

    fix phan xu ly them du lieu

--- api/controllers/create-data.js
+++ api/controllers/create-data.js
@@ -1,15 +1,15 @@
 module.exports = async function createData(req, res) {
-    let firstNames = ["Nguyen", "Tran", "Cao", "Ha", "Ngo", "Do"];
-    let secondNames = ["Thi", "Van", "Dinh", "Khanh", "Huu", "Duc"];
-    let thirdNames = ["Minh", "Minh", "Nam", "Ly", "Nhung", "Linh"];
-    let genders = ["Nam", "Nu"];
-    let addresses = ["Ha Noi", "Ho Chi Minh", "Da Nang", "Hue"];
+    let firstNames = ["Nguyễn", "Trần", "Cao", "Hà", "Ngô", "Đỗ"];
+    let secondNames = ["Thị", "Văn", "Đình", "Khánh", "Hữu", "Đức"];
+    let thirdNames = ["Minh", "Trang", "Nam", "Ly", "Nhung", "Linh"];
+    let genders = ["Nam", "Nữ"];
+    let addresses = ["Hà Nội", "Hồ Chí Minh", "Đà Nẵng", "Huế"];
 
     for (let i = 0; i < 500; i ++) {
         await Student.create({
             mssv: "2018" + ("0000" + i).slice(-4),
             name: firstNames[createRadomInRange(0, 6)] + " " + secondNames[createRadomInRange(0, 6)] + " " + thirdNames[createRadomInRange(0, 6)],
-            dateOfBirth: "20/01/1996",
+            dateOfBirth: ("00" + createRadomInRange(1, 31)).slice(-2) + "/" + ("00" + createRadomInRange(1, 13)).slice(-2) + "/1996" ,
             gender: genders[createRadomInRange(0, 2)],
             address: addresses[createRadomInRange(0, 4)]
         })

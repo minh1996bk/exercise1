a4be5b53439bb577bc2894e91d0ce2128bcaeaf9
minh1996bk <minh1996bk@gmail.com>
2018-04-21 21:11:00 +0700

    save

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,45 +1,95 @@
+var currentPage = 1;
+async function doAsync() {
+	async function getStudents(criteria) {
+		criteria = criteria || {};
+		let url = `/getStudents?
+			pageNumber=${criteria.pageNumber || 1}
+			&recordCount=${criteria.recordCount || 10}
+			&searchString=${criteria.searchString || ""}
+			&sortField=${criteria.sortField || 'mssv'}
+			&sortOrder=${criteria.sortOrder || 'ASC'}
+		`;
+		return await $.get(url)
+	}
 
-function check(){
-	var masv=document.getElementById('masv').value;
-	var loima=document.getElementById('error').value;
-	var hten=document.getElementById('txt_name').value;
-	var loiht=document.getElementById('errorht').value;
-	var ns=document.getElementById('txt_ns').value;
-	var loins=document.getElementById('errorns').value;
-	var dc=document.getElementById('txt_dc').value;
-	var loidc=document.getElementById('errordc').value;
-	var er;
+	function renderStudents(students) {
+		let htm;
+		students.forEach(student => {
+			htm += `
+			<tr>
+				<td>
+					<button><i class="glyphicon glyphicon-pencil"></i>Chỉnh sửa</button>
+					<button><i class="glyphicon glyphicon-remove"></i>Xóa</button>
+				</td>
+				<td>${student.mssv}</td>
+				<td>${student.name}</td>
+				<td>${student.dateOfBirth}</td>
+				<td>${student.gender}</td>
+				<td>${student.address}</td>
+			</tr>
+			`;
+		})
+		$('#tbodyRows').append(htm);
+	}
 
-	var pattern=/^[aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ\s]+$/;
-	er=/[0-9]{8}/.test(masv);
+	function renderNumberPage(totalStudent, currentPage) {
+		let totalPage = Math.ceil(totalStudent / 10);
+		currentPage = currentPage <= totalPage && currentPage >= 1 ? currentPage : 1;
+		let startIndex;
+		let endIndex;
+		let prev, next;
+		startIndex = currentPage - 4 >= 1 ? currentPage - 1 : 1;
+		endIndex = startIndex + 9 <= totalPage ? startIndex + 9 : totalPage;
+
+		let htm;
+		htm = `
+			<ul>
+				<li><button>First</button></li>
+				<li><button><<</button></li>
+		`;
+
+		for (let i = startIndex; i <= endIndex; i ++) {
+			htm += `
+				<li><button>${i}</button></li>
+			`;
+		}
+
+		htm += `
+				<li><button>>></button></li>
+				<li><button>Last</button></li>
+			</ul>
+		`;
+		$('#divPageNumber').append(htm);
+
+	}
+	(async function() {
+		let main = document.getElementById('main');
+
+		let popup = document.getElementById('divPopup');
+		
+		let btnInsert = document.getElementById('btnInsert');
 	
-	if(er==false){	
-			loima=document.getElementById('error').innerHTML="Mã sinh viên gồm 8 số";
-			return false;
+		let btnClosePopup = document.getElementById('btnClosePopup');
+	
+		btnInsert.onclick = function() {
+			main.style.display = "none";
+			popup.style.display = "block";
+	
+		}
+	
+		btnClosePopup.onclick = function() {
+			popup.style.display = "none";
+			main.style.display = "block";
+		}
 
-	}else  loima=document.getElementById('error').innerHTML="";
+		
+	})();
+	
 
-	if(pattern.test(hten)==false){	
-		loiht=document.getElementById('errorht').innerHTML="Họ tên không hợp lệ";
-		return false;
-
-	}else loiht=document.getElementById('errorht').innerHTML="";
-	if(ns==""){	
-		loins=document.getElementById('errorns').innerHTML="Không được để trống ngày sinh";
-		return false;
-
-	}else 	loins=document.getElementById('errorns').innerHTML="";
-
-	if(dc ==""){	
-		loidc=document.getElementById('errordc').innerHTML="Không được để trống địa chỉ";
-		return false;
-
-	}else loidc=document.getElementById('errordc').innerHTML="";
-	return true;
-
-
-
+	let data = await getStudents();
+		
+	renderStudents(data.students);
+	renderNumberPage(data.totalStudent, currentPage);
+	
 }
-
-
-
+doAsync();
